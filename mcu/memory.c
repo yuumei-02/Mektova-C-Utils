29d45781a882867f6c85f87a813667b9e35760cd
Yuumei <shjesna@protonmail.com>
2026-05-17 19:10:35 +0200

    Working arenas, memory protection doesn't seem to work tho

--- mcu/memory.c
+++ mcu/memory.c
@@ -1,6 +1,8 @@
 #include "core.h"
 #include "memory.h"
 
+#include <sys/mman.h>
+
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,5 +35,77 @@ void* mcu_realloc(void* ptr, usize new_byte_size) {
 
 void mcu_free(void* ptr) {
    free(ptr);
+}
+
+Arena Arena_new_ex(usize capacity, ArenaSettings settings) {
+   Arena self;
+   self.length = 0;
+   self.capacity = capacity;
+
+   i32 prot = 0;
+   if (settings.protection & MP_None) {
+      prot = PROT_NONE;
+   } else {
+      prot = settings.protection & MP_Exec  ? prot | PROT_EXEC  : prot;
+      prot = settings.protection & MP_Read  ? prot | PROT_READ  : prot;
+      prot = settings.protection & MP_Write ? prot | PROT_WRITE : prot;
+   }
+   
+   self.buffer = mmap(nullptr, capacity, prot, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+   if (self.buffer == MAP_FAILED) {
+      panic("[!] Failed to allocate memory for arena of size \"%zu\", reason: \"%s\"", capacity, strerror(errno));
+   }
+   
+   return self;
+}
+
+void Arena_destroy(nullable Arena* self) {
+   if (self == nullptr) return;
+
+   munmap(self->buffer, self->capacity);
+   *self = (Arena) {0};
+}
+
+void* Arena_alloc(Arena* self, usize bytes) {
+   mcu_assert(self != nullptr, "self can't be null");
+
+   if (bytes > (self->capacity - self->length)) {
+      panic("Allocation from arena failed with OOM");
+   }
+
+   void* data = self->buffer + self->length;
+   self->length += bytes;
+
+   return data;
+}
+
+/// Same as [Arena_alloc] except that this version returns [null] on allocation failure.
+void* Arena_alloc_non_panic(Arena* self, usize bytes) {
+   mcu_assert(self != nullptr, "self can't be null");
+
+   if (bytes > (self->capacity - self->length)) {
+      return nullptr;
+   }
+
+   void* data = self->buffer + self->length;
+   self->length += bytes;
+   
+   return data;
+}
+
+/// [bytes] is allowed to be more than the [arena's length].
+void Arena_free(nullable Arena* self, usize bytes) {
+   if (self == nullptr) return;
+
+   if (self->length < bytes) {
+      self->length = 0;
+   } else {
+      self->length -= bytes;
+   }
+}
+
+void Arena_clear(nullable Arena* self) {
+   if (self == nullptr) return;
+   self->length = 0;
 }
 
--- mcu/memory.h
+++ mcu/memory.h
@@ -19,3 +19,36 @@ void* mcu_realloc(void* ptr, usize new_byte_size);
 /// [ptr] is allowed to be [null].
 void mcu_free(void* ptr);
 
+typedef enum : i32 {
+   MP_Exec  = 1,
+   MP_Read  = 2,
+   MP_Write = 4,
+   MP_None  = 0,
+} MemoryProtection;
+
+typedef struct {
+   u8* buffer;
+   usize length;
+   usize capacity;
+} Arena;
+
+typedef struct {
+   MemoryProtection protection;
+} ArenaSettings;
+
+#define Arena_new(capacity, ...) \
+   Arena_new_ex(capacity, (ArenaSettings) { \
+      .protection = MP_Read | MP_Write \
+   })
+
+Arena Arena_new_ex(usize capacity, ArenaSettings settings);
+void Arena_destroy(nullable Arena* self);
+
+void* Arena_alloc(Arena* self, usize bytes);
+/// Same as [Arena_alloc] except that this version returns [null] on allocation failure.
+void* Arena_alloc_non_panic(Arena* self, usize bytes);
+
+/// [bytes] is allowed to be more than the [arena's length].
+void Arena_free(nullable Arena* self, usize bytes);
+void Arena_clear(nullable Arena* self);
+
